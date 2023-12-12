26313f0d5ffc90f2386c695813cb52ca1e7b6eee
Dominic <domcashiola@gmail.com>
2023-12-12 15:13:54 -0500

    Styling board

--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,18 +1,32 @@
 class View {
   constructor(game, el) {
-  }
-  
-  setupBoard() {
-  }
-  
-  handleClick(e) {
+    this.game = game;
+    this.el = el;
+
+    this.setupBoard();
   }
 
-  makeMove(square) {
+  setupBoard() {
+    const board = document.createElement("ul");
+
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        const cell = document.createElement("li");
+        cell.dataset.row = row;
+        cell.dataset.col = col;
+
+        board.appendChild(cell);
+      }
+    }
+
+    this.el.appendChild(board);
   }
-  
-  handleGameOver() {
-  }
+
+  handleClick(e) {}
+
+  makeMove(square) {}
+
+  handleGameOver() {}
 }
 
 export default View;
